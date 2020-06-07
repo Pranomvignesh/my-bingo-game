44fb6fab7c0c5dc489ae9e51e5820071325113b5
pranomvignesh <pranomvignesh@gmail.com>
2020-06-07 22:35:16 +0530

    created my own server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const path          = require('path');
+const http          = require('http');
+const app           = express();
+const PORT          = process.env.port||4000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/',express.static(path.join(__dirname,'dist')));
+app.get('/',function(request,response){
+    response.send(path.join(__dirname,'dist/index.html'));
+})
+http.createServer(app).listen(PORT);
+console.log(`Server running in http://localhost:${PORT}/`);
